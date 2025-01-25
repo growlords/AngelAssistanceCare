48a8842853e5ebc604ac7c58ea0576d4027f97d4
sahil <sharmasahil0118@gmail.com>
2025-01-25 19:34:18 +0530

    Done

--- Frontend/src/components/Home_2.jsx
+++ Frontend/src/components/Home_2.jsx
@@ -52,7 +52,7 @@ const Home_2 = () => {
         height: '100%',  // Ensures the div takes up the full viewport height
       }}
     >
-      <div className='flex flex-col sm:flex-row md:flex-col-reverse  lg:flex-row py-[5rem]'>
+      <div className='flex flex-col sm:flex-row md:flex-col-reverse  lg:flex-row py-[5rem] lg:py-0'>
         <div className="left flex-1 flex justify-center sm:justify-start md:justify-center lg:justify-start">
           <div className="img md:mt-[5rem]">
             <motion.img
